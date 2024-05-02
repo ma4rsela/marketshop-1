1e83e6e68d2458c15459e2b21e97c2b80e54865d
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2024-05-02 10:48:10 -0300

    comando inicia aula, salvar usuario no banco, tabela de produtos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 
 Route::get('/home', function () {
@@ -16,6 +17,14 @@ Route::view('/testedeconteudo', 'teste');
 
 Route::post('/salva-usuario',
 function (Request $request) {
-    dd($request);
+    // dd($request);
+
+    $usuario = new User();
+    $usuario->name = $request->nome;
+    $usuario->email = $request->email;
+    $usuario->password = $request->senha;
+    $usuario->save();
+    dd("Salvo com sucesso!!");
+
 })->name('salva-usuario');
 
