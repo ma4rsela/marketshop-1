90e4548b391cb5775a5dfd05b10f19b440da8647
luansilvafatec <luan.silva99@fatec.sp.gov.br>
2024-05-02 07:45:28 -0300

    fazer botão do formulário salvar, e festa do samuel'

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
+
+Route::get('/home', function () {
     return view('welcome');
 });
 
-Route::view('/home', 'home');
+Route::view('/', 'home');
+
+Route::view('/cria-conta', 'cria-conta');
 
 Route::view('/testedeconteudo', 'teste');
+
+Route::post('/salva-usuario',
+function (Request $request) {
+    dd($request);
+})->name('salva-usuario');
 
